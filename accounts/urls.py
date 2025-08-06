e54725a8075733c59da5cd11159fa277ce043f6f
Tagir-Alisheykhov <ahitahir@yandex.ru>
2025-08-06 18:47:59 +0300

    Initialized Project

---
+++ accounts/urls.py
@@ -0,0 +1,11 @@
+"""
+Конечные точки приложения `accounts`.
+"""
+
+from django.urls import path
+from accounts.apps import AccountsConfig
+
+app_name = AccountsConfig.name
+
+urlpatterns = [
+]
---
+++ config/settings.py
@@ -0,0 +1,153 @@
+import os
+from datetime import timedelta
+from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+
+DEBUG = True if os.getenv("DEBUG") == "True" else False
+
+ALLOWED_HOSTS = ["*"]
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    "rest_framework_simplejwt",
+    "rest_framework",
+    "django_filters",
+    "corsheaders",
+    "drf_yasg",
+    'accounts',
+]
+
+MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'config.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+
+REST_FRAMEWORK = {
+    "DEFAULT_FILTER_BACKENDS": (
+        "django_filters.rest_framework.DjangoFilterBackend"
+    ),
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
+    "DEFAULT_PERMISSION_CLASSES": (
+        "rest_framework.permissions.IsAuthenticated",
+    ),
+}
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=1440),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+}
+
+
+WSGI_APPLICATION = 'config.wsgi.application'
+
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": os.getenv("POSTGRES_DB", "book_library_spa"),
+        "USER": os.getenv("POSTGRES_USER", "postgres"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD", "password"),
+        "HOST": os.getenv("POSTGRES_HOST", "db"),
+        "PORT": os.getenv("POSTGRES_PORT", "5432"),
+    }
+}
+
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+
+LANGUAGE_CODE = 'en-us'
+TIME_ZONE = 'Europe/Moscow'
+USE_I18N = True
+USE_TZ = True
+
+STATIC_URL = 'static/'
+STATIC_ROOT = "/static"
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+AUTH_USER_MODEL = "accounts.User"
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CELERY_BEAT_SCHEDULE = {
+    "deactivate-inactive-users": {
+        "task": "habits.tasks.check_and_send_habit_reminders",
+        "schedule": timedelta(minutes=15),
+    },
+}
+
+
+# Настройка CORS
+CORS_ALLOW_ALL_ORIGINS = (
+    os.getenv(str("CORS_ALLOW_ALL_ORIGINS"), "False").lower() == "true"
+)
+
+if not CORS_ALLOW_ALL_ORIGINS:
+    cors_frontend = os.getenv(os.getenv("CORS_FRONTEND"), "")
+    if cors_frontend:
+        # Разделяем строку по запятым и убираем лишние пробелы
+        CORS_ALLOWED_ORIGINS = [
+            origin.strip() for origin in cors_frontend.split(",") if origin.strip()
+        ]
+    else:
+        CORS_ALLOWED_ORIGINS = []
+else:
+    CORS_ALLOWED_ORIGINS = []
+csrf_origins = os.getenv("CORS_FRONTEND", "")
+if csrf_origins:
+    CSRF_TRUSTED_ORIGINS = [
+        origin.strip() for origin in csrf_origins.split(",") if origin.strip()
+    ]
+else:
+    CSRF_TRUSTED_ORIGINS = []
